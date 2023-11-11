d2916aed4d2861f27302a9aa1e403a114fed647c
Kevin <kevinjxhn@Kevins-MacBook-Air.local>
2023-11-11 15:18:25 +0530

    Minor changes

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
-const desktopButton = document.querySelectorAll('.location-button');
+const desktopButton = document.querySelector(".location-button");
 
-desktopButton.forEach(button => {
-  button.addEventListener('click', function() {
-    window.location.href = 'location.html';
-  });
+desktopButton.addEventListener("click", function () {
+  window.location.href = "location.html";
 });
 
-const tabletButton = document.querySelectorAll('.location-button-tablet');
+const tabletButton = document.querySelector(".location-button-tablet");
 
-tabletButton.forEach(button => {
-  button.addEventListener('click', function() {
-    window.location.href = 'location.html';
-  });
+tabletButton.addEventListener("click", function () {
+  window.location.href = "location.html";
 });
 
-const mobileButton = document.querySelectorAll('.location-button-mobile');
+const mobileButton = document.querySelector(".location-button-mobile");
 
-mobileButton.forEach(button => {
-  button.addEventListener('click', function() {
-    window.location.href = 'location.html';
-  });
-});
-
-
-const homeButton = document.querySelector('#home');
-homeButton.addEventListener('click', function() {
-  window.location.href = 'index.html';
+mobileButton.addEventListener("click", function () {
+  window.location.href = "location.html";
 });
